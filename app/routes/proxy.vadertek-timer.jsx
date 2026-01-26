3b1ca0aeccd20a6d5f433332f464ea2ff857df2b
Pavlo Leiko <drawnooblive@gmail.com>
2026-01-26 19:10:06 +0200

    Update

--- app/routes/proxy.vadertek-timer.jsx
+++ app/routes/proxy.vadertek-timer.jsx
@@ -264,7 +264,7 @@ export async function action({ request }) {
       `,
         {
           variables: {
-            input: { email: emailValue },
+            input: { email: emailValue, tags: ["wheel-customer"] },
           },
         },
       );
